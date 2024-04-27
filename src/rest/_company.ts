df3dd9064a86b8526df0dfc74549c0c71739c903
Scr33ny <111527738+Scr33ny@users.noreply.github.com>
2024-04-28 00:20:18 +0200

    Refactor company rest laag

--- src/rest/_company.ts
+++ src/rest/_company.ts
@@ -1,20 +1,20 @@
-import Koa from 'koa';
-import Router from '@koa/router';
+const Koa = require('koa');
+const Router = require('@koa/router');
 const { requireAuthentication, makeRequireRole } = require('../core/auth');
-import { Role } from '../core/roles';
+const { Role } = require('../core/roles');
 
-import companyService from '../service/company';
+const companyService = require('../service/company');
 
-const getOwnProducts = async (ctx: Koa.Context) => {
+const getOwnProducts = async (ctx: typeof Koa.Context) => {
   const { companyId } = ctx.state.session;
   ctx.body = await companyService.getAllProducts(companyId);
 };
 
-const getAllCompanies = async (ctx: Koa.Context) => {
+const getAllCompanies = async (ctx: typeof Koa.Context) => {
   ctx.body = await companyService.getAllCompanies();
 };
 
-export default function installCompanyRouter(app: Router) {
+export default function installCompanyRouter(app: typeof Router) {
   const router = new Router({
     prefix: '/company',
   });

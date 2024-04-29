d9330fcc39692f17908f3fd79196ba781e195f64
BrechtVanderhoydonck <brecht.vanderhoydonck@student.hogent.be>
2024-04-29 21:42:42 +0200

    SDP2324G02-109 REFACTORING ./core

--- src/core/jwt.ts
+++ src/core/jwt.ts
@@ -1,58 +1,67 @@
-import { JsonWebKey } from "crypto";
-import { JsonWebTokenError } from "jsonwebtoken";
-import { serializedAccount } from "../data/user"
+import { JsonWebTokenError, VerifyOptions, SignOptions } from "jsonwebtoken";
+import { serializedAccount } from "../data/user";
+import Jwt from "jsonwebtoken";
+import config from "config";
 
-const jwt = require('jsonwebtoken');
-
-const config = require('config');
-
-const JWT_AUDIENCE = config.get('auth.jwt.audience');
-const JWT_SECRET = config.get('auth.jwt.secret');
-const JWT_ISSUER = config.get('auth.jwt.issuer');
-const JWT_EXPIRATION_INTERVAL = config.get('auth.jwt.expirationInterval');
+const JWT_AUDIENCE: string = config.get("auth.jwt.audience");
+const JWT_SECRET: Jwt.Secret = config.get("auth.jwt.secret");
+const JWT_ISSUER: string = config.get("auth.jwt.issuer");
+const JWT_EXPIRATION_INTERVAL: number = config.get(
+  "auth.jwt.expirationInterval"
+);
 
 const generateJWT = (acc: serializedAccount) => {
-
-  const tokenData = {
+  const tokenData: object = {
     userId: acc.id,
     email: acc.email,
     role: acc.role,
     companyId: acc.companyId,
   };
 
-  const signOptions = {
+  const signOptions: SignOptions = {
     expiresIn: Math.floor(JWT_EXPIRATION_INTERVAL / 1000),
     audience: JWT_AUDIENCE,
     issuer: JWT_ISSUER,
-    subject: 'auth',
+    subject: "auth",
   };
 
   return new Promise((resolve, reject) => {
-    jwt.sign(tokenData, JWT_SECRET, signOptions, (err: JsonWebTokenError, token: JsonWebKey) => {
-      if (err) {
-        console.log('Error while signing new token:', err.message);
-        return reject(err);
+    Jwt.sign(
+      tokenData,
+      JWT_SECRET,
+      signOptions,
+      (err: Error | null, token: string | undefined) => {
+        if (err) {
+          console.log("Error while signing new token:", err.message);
+          return reject(err);
+        }
+        return resolve(token);
       }
-      return resolve(token);
-    });
+    );
   });
 };
 
-const verifyJWT = (authToken: JsonWebKey) => {
-  const verifyOptions = {
+const verifyJWT = (authToken: string) => {
+  const verifyOptions: VerifyOptions & { complete: true } = {
     audience: JWT_AUDIENCE,
     issuer: JWT_ISSUER,
-    subject: 'auth',
+    subject: "auth",
+    complete: true,
   };
 
   return new Promise((resolve, reject) => {
-    jwt.verify(authToken, JWT_SECRET, verifyOptions, (err: JsonWebTokenError, decodedToken: JsonWebKey) => {
-      if (err || !decodedToken) {
-        console.log('Error while verifying token:', err.message);
-        return reject(err || new Error('Token could not be parsed'));
+    Jwt.verify(
+      authToken,
+      JWT_SECRET,
+      verifyOptions,
+      (err: Error | null, decodedToken: object | undefined) => {
+        if (err || !decodedToken) {
+          console.log("Error while verifying token:", err?.message);
+          return reject(err || new Error("Token could not be parsed"));
+        }
+        return resolve(decodedToken);
       }
-      return resolve(decodedToken);
-    });
+    );
   });
 };
 
--- src/core/password.ts
+++ src/core/password.ts
@@ -1,6 +1,5 @@
-import { Hash } from "crypto";
-
-const crypto = require('crypto');
+import { Hash, createHash } from "crypto";
+import { getLogger } from "./logging";
 
 function encryptPassword(password: string, salt: Buffer) {
   try {
@@ -8,22 +7,23 @@ function encryptPassword(password: string, salt: Buffer) {
     const fullPassword = Buffer.concat([Buffer.from(password), salt]);
 
     // Hash the combined buffer using SHA-256
-    const hashedBytes = crypto.createHash('sha256').update(fullPassword).digest();
+    const hashedBytes = createHash("sha256").update(fullPassword).digest();
 
     // Convert the hashed bytes to a hexadecimal string
-    const hashedPassword = hashedBytes.toString('hex');
+    const hashedPassword = hashedBytes.toString("hex");
 
     return hashedPassword;
   } catch (error) {
-    console.error('Error hashing password:', error);
-    return null;
+    getLogger().error("Error hashing password: ", error);
   }
 }
-const customSalt = Buffer.from('J#7pQzL9', 'utf-8');
-const hashedPassword = (password: string): string => { return encryptPassword(password, customSalt);}
+const customSalt = Buffer.from("J#7pQzL9", "utf-8");
+const hashedPassword = (password: string): string | undefined => {
+  return encryptPassword(password, customSalt);
+};
 
 const verifyPassword = async (password: string, passwordHash: string) => {
-  const valid = hashedPassword(password) == passwordHash
+  const valid = hashedPassword(password) == passwordHash;
 
   return valid;
 };

a9d18b95e4f39ee1c5fe394d0ab3c7f662bae0b0
Arno Heekhout <114562521+arno-heekhout@users.noreply.github.com>
2024-05-06 09:47:00 +0200

    Current company ophalen

--- src/rest/_company.ts
+++ src/rest/_company.ts
@@ -2,6 +2,7 @@ import Koa from "koa";
 import Router from "@koa/router";
 
 import * as companyService from "../service/company";
+import { requireAuthentication } from "../core/auth";
 
 const getOwnProducts = async (ctx: Koa.Context) => {
   const companyId = ctx.params.id;
@@ -15,6 +16,7 @@ const getAllCompanies = async (ctx: Koa.Context) => {
 const getCompany = async (ctx: Koa.Context) => {
   // WAAROM KAN IK NIET AAN SESSION??????????????? VR DE REST WEL OK DENK IK
   // PAS HIERNA DE INVOICE AAN
+  // --> Kan er nu wel aan bij gebruik vn requireAuthentication
 
   // er staat wel alleen klant bij de rol van factuur downloaden
   // dus daarvoor tonen we de bestellingen die HIJ heeft geplaatst bij andere bedrijven
@@ -22,7 +24,7 @@ const getCompany = async (ctx: Koa.Context) => {
   const companyId = ctx.params.id.toLowerCase() === "current"
     ? ctx.state.session.companyId
     : ctx.params.id;
-
+  
   ctx.body = await companyService.getCompany(companyId);
 };
 
@@ -31,12 +33,11 @@ export default function installCompanyRouter(app: Router) {
     prefix: "/company",
   });
 
-  //const requireSupplier = makeRequireRole(Role.SUPPLIER);
-
+  // TODO: zorgt voor requireAuthentication op /:id vr problemen op de frontend?
   // Public routes
   router.get("/:id/products", getOwnProducts);
   router.get("/", getAllCompanies);
-  router.get("/:id", getCompany)
+  router.get("/:id", requireAuthentication, getCompany)
 
   app.use(router.routes()).use(router.allowedMethods());
 }

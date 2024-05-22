2dcaff65ce66cce1a113aa47d7ab1753aeebfb33
BrechtVanderhoydonck <brecht.vanderhoydonck@student.hogent.be>
2024-05-22 22:09:03 +0200

    Chatbox toont terug zijn logs

--- src/createServer.ts
+++ src/createServer.ts
@@ -37,8 +37,8 @@ export default async function createServer() {
   await initializeData();
 
   // train nlp model
-  //await nlpManager.train();
-  //nlpManager.save();
+  await nlpManager.train();
+  nlpManager.save();
 
   // Create a new KOA App
   const app = new Koa();
